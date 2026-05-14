23b047ec5599346d722301baf608891d857b861a
robexee <davidrobertmihai@gmail.com>
2026-05-14 12:18:34 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+// Definirea pinilor
+const int buzzerPin = 2; // Conectează firul de semnal al buzzerului aici
+const int led1Pin = 23;  // Conectează Anodul (+) primului LED aici
+const int led2Pin = 22;  // Conectează Anodul (+) celui de-al doilea LED aici
+
+void setup() {
+}
+
+void loop() {
+
+}
